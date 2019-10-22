3f5690f57c1e4a8e06e5809ae031f5525ef638c1
Joel Diaz <jdiaz5895@gmail.com>
2019-10-22 10:42:14 -0400

    final commit

--- main.js
+++ main.js
@@ -71,8 +71,22 @@ function simpleReverse(str) {
 // Q7: Write the function findDiff(arr) taking an array of numbers as parameter and return the difference between the maximum number and the minimum number (max - min).
 
 function findDiff(arr) {
-  
+  var min = arr[0];
+  var max = arr[0];
+  var length = arr.length;
+​
+  for (var i = 0; i < length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max - min;
 }
+
+// console.log(findDiff([1, 2, 4, 6, 20, 3]));
 
 // Q8: Write the function timeConvert(num) taking a number as parameter and return the number of hours and minutes the parameter converts to. Separate the number of hours and minutes with a colon.
 
@@ -87,16 +101,77 @@ function timeConvert(num) {
 
 // Q9: Write the function findStr(str, long) taking two strings as parameters and return how many str you can find in long. Assume Str is not empty string.
 
-function findStr(str, long)
+function findStr(str, long) {
+  var longLength = long.length;
+  var strLength = str.length;
+  var counter = 0;
+  for (var i = 0; i <= longLength - strLength; i++) {
+    if (str === long.slice(i, i + str.length)) {
+      counter++;
+    }
+  }
+  return counter;
+}
+
+// console.log(findStr("hi", "yerrr"));
 
 // Q10: Write the function selfDividingNumbers(left, right) taking two number bound as parameters and returns an array of every possible self dividing number between them, including the bounds.
 
-function selfDividingNumbers(left, right)
+function selfDividingNumbers(left, right) {
+  var solution = [];
+  for (var i = left; i <= right; i++) {
+    if (checkSelfDividing(i)) {
+      solution.push(i);
+    }
+  }
+​
+  return solution;
+}
+
+function checkSelfDividing(num) {
+  var numString = num.toString();
+  var numArray = numString.split("");
+​
+  var length = numArray.length;
+  for (var i = 0; i < length; i++) {
+    if (num % ParseInt(numArray[i]) !== 0) {
+      return false;
+    }
+  }
+  return true;
+}
 
 // Q11: Write the function moveZeros(nums) taking an array of numbers and move all 0’s to the end of it while maintaining the relative order of the non-zero elements.
 
-function moveZeros(nums)
+function moveZeros(nums) {
+  var solution = [];
+  var counter = 0;
+  var length = nums.length;
+  // loop through the array
+  for (var i = 0; i < length; i++) {
+    if (nums[i] === 0) {
+      counter++;
+    } else {
+      solution.push(nums[i]);
+    }
+  }
+​
+  // adding 0s
+  while (counter > 0) {
+    solution.push(0);
+    counter--;
+  }
+​
+  return solution;
+}
 
 // Q12: Create an average(nums) function that calculates the average of an array of numbers.
 
-function average(nums)
+function average(nums){
+  var sum = 0;
+  var length = nums.length;
+  for (var i = 0; i < length; i++) {
+    sum += nums[i];
+  }
+  return sum / length;
+}
